50f644e807be01d0175aef65150d67f1fb583cea
noorahmad <noorahmad23@hotmail.com>
2017-07-17 12:20:17 -0700

    created home and about components

---
+++ src/components/about.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () =>
+<section className="one wrapper style1 special">
+	<div className="inner">
+		<header className="major">
+			<h2>Arcu aliquet vel lobortis ata nisl<br />
+				eget augue amet aliquet nisl cep donec</h2>
+				<p>Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet eleifend<br />
+					fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus ullamcorper.</p>
+		</header>
+			<ul className="icons major">
+				<li><span className="icon fa-diamond major style1"><span className="label">Lorem</span></span></li>
+				<li><span className="icon fa-heart-o major style2"><span className="label">Ipsum</span></span></li>
+				<li><span className="icon fa-code major style3"><span className="label">Dolor</span></span></li>
+			</ul>
+	</div>
+</section>
+
+export default About
---
+++ src/components/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => 
+<section id="banner">
+	<div className="inner">
+		<h2>Noor Ahmad</h2>
+		<p>Software Engineer</p>
+		<ul className="actions">
+			<li><a href="#" className="button special">Activate</a></li>
+		</ul>
+	</div>
+	<a href="#one" className="more scrolly">Learn More</a>
+</section>
+
+export default Home
