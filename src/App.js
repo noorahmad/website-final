133ba1925248bfe1c49f4b65928337ec7469e8d8
noorahmad <noorahmad23@hotmail.com>
2017-07-17 13:41:45 -0700

    finished adding rest of the components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import Home from './components/home';
+import Home from './components/home'
 import About from './components/about'
+import Portfolio from './components/portfolio'
+import Navbar from './components/navbar'
+import Competent from './components/competent'
+import Footer from './components/footer'
+
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Navbar />
         <Home />
         <About />
+        <Portfolio />
+        <Competent />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () =>
+
+<footer id="footer">
+	<ul className="icons">
+		<li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+		<li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+		<li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+		<li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
+		<li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+	</ul>
+	<ul className="copyright">
+		<li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+	</ul>
+</footer>
+
+export default Footer
